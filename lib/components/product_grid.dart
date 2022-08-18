a1f6dfc883129c311d916a4cbed8f76142e2b568
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2022-08-18 11:58:07 -0300

    first commit

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+
+import '../models/product.dart';
+import 'product_grid_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  final bool showFavoriteOnly;
+
+  ProductGrid(this.showFavoriteOnly);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts =
+        showFavoriteOnly ? provider.favoriteItems : provider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      itemBuilder: (ctx, idx) => ChangeNotifierProvider.value(
+        value: loadedProducts[idx],
+        child: ProductGridItem(),
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'url/products';
+  static const USER_FAVORITES_URL =
+      'url/userFavorites';
+  static const ORDER_BASE_URL =
+      'url/orders';
+}
